4a84942e88a24cf5e0a017cf802a9c72324e211a
SvetlanaNikonova <SvetlanaNikonova@coherentsolutions.com>
2022-04-06 09:48:36 +0300

    changed final

--- consoleApp/src/main/java/com/issoft/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/com/issoft/consoleApp/StoreApp.java
@@ -1,33 +1,17 @@
 package com.issoft.consoleApp;
 
-import com.issoft.domain.Category;
-import com.issoft.domain.Product;
-import com.issoft.domain.categories.BikeCategory;
-import com.issoft.domain.categories.MilkCategory;
-import com.issoft.domain.categories.PhoneCategory;
+
 import com.issoft.store.Store;
+import com.issoft.store.helpers.StoreHelper;
 
 public class StoreApp {
 
 public static void main(String[] args){
+    Store onlineStore = new Store();
+    StoreHelper storeHelper = new StoreHelper(onlineStore);
+    storeHelper.fillStoreRandomly();
+    onlineStore.printAllCategoriesAndProducts();
 
-Store store = new Store();
-Category bikes = new BikeCategory();
-Category milk = new MilkCategory();
-Category phone = new PhoneCategory();
-store.addCategory(bikes);
-store.addCategory(milk);
-store.addCategory(phone);
-
-Product superBike = new Product("Panda", 1000.0, 15.0);
-Product milkMu = new Product("Muu", 1.99, 0.5);
-Product phoneApple = new Product("SE", 800.5, 2.5);
-
-bikes.addProduct(superBike);
-milk.addProduct(milkMu);
-phone.addProduct(phoneApple);
-
-store.printAllCategoriesAndProducts();
 
 }
 
--- store/src/main/java/com/issoft/store/helpers/StoreHelper.java
+++ store/src/main/java/com/issoft/store/helpers/StoreHelper.java
@@ -22,31 +22,31 @@ public class StoreHelper {
     public  void  fillStoreRandomly() {
 
         RandomStorePopulator populator = new RandomStorePopulator();
-        Map<Category, Integer> categoryProductsMapToAdd = createProductListToAdd();
+        Map<Category, Integer> categoryMap = createCategoryMap();
 
-        for (Map.Entry<Category, Integer> entry : categoryProductsMapToAdd.entrySet()) {
+        for (Map.Entry<Category, Integer> entry : categoryMap.entrySet()) {
             for (int i = 0; i < entry.getValue(); i++) {
 
                 Product product = new Product(
                         populator.getProductName(entry.getKey().getName()),
                         populator.getPrice(),
                         populator.getRate());
-                entry.getKey().addProduct(product);
+                entry.getKey().addProductToCategory(product);
             }
             this.store.addCategory(entry.getKey());
     }
     }
-    private static Map<Category, Integer> createProductListToAdd() {
-        Map<Category, Integer> productsToAdd = new HashMap<>();
+    private static Map<Category, Integer> createCategoryMap() {
+        Map<Category, Integer> categoryToPut = new HashMap<>();
 
         Reflections reflections = new Reflections("com.issoft.domain.categories");
 
-        Set<Class<? extends Category>> subtypes = reflections.getSubTypesOf(Category.class);
+        Set<Class<? extends Category>> subTypes = reflections.getSubTypesOf(Category.class);
 
-        for (Class<? extends  Category> type : subtypes) {
+        for (Class<? extends  Category> type : subTypes) {
             try {
                 Random random = new Random();
-                productsToAdd.put(type.getConstructor().newInstance(), random.nextInt(10));
+                categoryToPut.put(type.getConstructor().newInstance(), random.nextInt(10));
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
@@ -57,7 +57,7 @@ public class StoreHelper {
                 e.printStackTrace();
             }
         }
-        return  productsToAdd;
+        return categoryToPut;
 
 
         }
